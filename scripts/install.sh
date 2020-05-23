386f988b2d670518e93e16cd245d479f02777176
Farna Chilashvili <farnnn.ff@gmail.com>
2020-05-24 03:06:40 +0400

    Add .env copy step.

--- scripts/install.sh
+++ scripts/install.sh
@@ -12,24 +12,26 @@ NONE='\033[00m'
 RED='\033[0;31m'
 BLUE='\033[1;36m'
 YELLOW='\033[1;33m'
+STEPS=6
 
 clear
 
-echo -e "${RED}[1/5] Stopping containers...\n\v${NONE}"
+echo -e "${RED}[1/${STEPS}] Stopping containers...\n\v${NONE}"
 make stop
 
-echo -e "${RED}[2/5] Checking and cloning repos...\n\v${NONE}"
+echo -e "${RED}[2/${STEPS}] Checking and cloning repos...\n\v${NONE}"
 [[ -d shwitter-com-angular ]] || git clone -b develop ${REACT_REPO}
 [[ -d shwitter-com-nodeJS ]] || git clone -b develop ${API_REPO}
+[[ -f shwitter-com-nodeJS/.env ]] || cp shwitter-com-nodeJS/.env.dist shwitter-com-nodeJS/.env
 
-echo -e "${RED}[3/5] Starting up containers...\n\v${NONE}"
+echo -e "${RED}[3/${STEPS}] Starting up containers...\n\v${NONE}"
 make up
 
-echo -e "\n${RED}[4/5] Building API ...\n\v${NONE}"
+echo -e "\n${RED}[4/${STEPS}] Building API ...\n\v${NONE}"
 docker-compose exec shwitter.api chown -R node:node ../node
 docker-compose exec --user node shwitter.api npm install
 
-echo -e "\n${RED}[5/5] Building Frontend...\n\v${NONE}"
+echo -e "\n${RED}[5/${STEPS}] Building Frontend...\n\v${NONE}"
 docker-compose exec shwitter.frontend chown -R node:node ../node
 docker-compose exec --user node shwitter.frontend npm install
 
